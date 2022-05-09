421dfd58ddf0c2f27dc87c9256ecb1892a05fe66
aliNejati-bu <electrocellco@gmail.com>
2022-05-09 16:48:19 +0430

    add database schemas

--- Database/Seed/UserSeeder.php
+++ Database/Seed/UserSeeder.php
@@ -8,8 +8,6 @@ class UserSeeder
 {
     public static function Seed()
     {
-        User::create([
-            "app" => "ali"
-        ]);
+        
     }
 }
--- Database/schemas.php
+++ Database/schemas.php
@@ -4,7 +4,103 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 Capsule::schema()->create('users', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
     $blueprint->id();
-    $blueprint->string("app");
+    $blueprint->string("user_email")->unique();
+    $blueprint->string("password");
+    $blueprint->string("phone");
+    $blueprint->integer("user_type");
+    $blueprint->boolean("is_phone_verified");
+    $blueprint->boolean("is_email_verified");
+    $blueprint->boolean("is_super_admin");
     $blueprint->timestamps();;
 });
 
+Capsule::schema()->create('phone_codes', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("code");
+    $blueprint->string("expire_at");
+    $blueprint->boolean("is_usesd");
+    $blueprint->bigInteger("user_id")->unsigned();
+    $blueprint->timestamps();
+    $blueprint->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+});
+
+Capsule::schema()->create('email_links', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->dateTime('expire_at');
+    $blueprint->boolean('is_used');
+    $blueprint->bigInteger("user_id")->unsigned();
+    $blueprint->timestamps();
+    $blueprint->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+});
+
+Capsule::schema()->create('slugs', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("slug")->unique();
+    $blueprint->boolean("is_direct");
+    $blueprint->bigInteger("user_id")->unsigned();
+    $blueprint->timestamps();
+    $blueprint->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+});
+
+Capsule::schema()->create('links', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("target_link", 1000);
+    $blueprint->bigInteger('slug_id')->unsigned();
+    $blueprint->timestamps();
+    $blueprint->foreign("slug_id")->references("id")->on("slugs")->onDelete("cascade");
+
+
+});
+
+
+Capsule::schema()->create('clicks', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("clicker_ip")->index();
+    $blueprint->bigInteger('slug_id')->unsigned();
+    $blueprint->bigInteger("link_id")->unsigned();
+    $blueprint->string("refer", 1000);
+    $blueprint->timestamps();
+
+    $blueprint->foreign("slug_id")->references("id")->on("slugs")->onDelete("cascade");
+    $blueprint->foreign("link_id")->references("id")->on("links")->onDelete("cascade");
+
+});
+
+
+Capsule::schema()->create('news', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("slug")->unique();
+    $blueprint->bigInteger("user_id")->unsigned();
+    $blueprint->string("title");
+    $blueprint->text("content");
+    $blueprint->timestamps();
+    $blueprint->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+
+});
+
+Capsule::schema()->create('roles', function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("role_name");
+    $blueprint->timestamps();
+});
+
+Capsule::schema()->create("role_user", function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->bigInteger("user_id")->unsigned();
+    $blueprint->bigInteger("role_id")->unsigned();
+    $blueprint->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+    $blueprint->foreign("role_id")->references("id")->on("roles")->onDelete("cascade");
+});
+
+Capsule::schema()->create("permissions", function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->id();
+    $blueprint->string("permission_name");
+    $blueprint->string("persian_name");
+    $blueprint->timestamps();
+});
+
+Capsule::schema()->create("permission_role", function (\Illuminate\Database\Schema\Blueprint $blueprint) {
+    $blueprint->bigInteger("permission_id")->unsigned();
+    $blueprint->bigInteger("role_id")->unsigned();
+    $blueprint->foreign("permission_id")->references("id")->on("permissions")->onDelete("cascade");
+    $blueprint->foreign("role_id")->references("id")->on("roles")->onDelete("cascade");
+});
