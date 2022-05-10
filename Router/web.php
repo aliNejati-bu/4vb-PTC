b466c3e567c9e7d9b193051787bdeb62f10abae0
aliNejati-bu <electrocellco@gmail.com>
2022-05-10 20:50:01 +0430

    add email sender

--- Router/web.php
+++ Router/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Phroute\Phroute\RouteCollector;
+
 /**
  * @var RouteCollector $router
  */
 
-$router->get("/",function (){
+$router->get("/", function () {
+    var_dump(\PTC\Classes\Mail::getInstance()->send(["electrocellco9@gmail.com"], "test Email", "PHP MAILER", "hi ali", "html not supported,"));
+    die();
     $name = "Welcome";
-    return view("index",compact("name"));
+    return view("index", compact("name"));
 });
