dd6a4a9ee1d230ea3bbf3a3b0d10a97671c7a25a
aliNejati-bu <electrocellco@gmail.com>
2022-05-29 09:13:05 +0430

    add only verify phone number middleware.

--- Boot/kernelOptions.php
+++ Boot/kernelOptions.php
@@ -5,6 +5,7 @@ return [
     'middleware' => [
         'exampleMiddleware' => \PTC\App\Middleware\ExampleMiddleware::class,
         'authMiddleware' => \PTC\App\Middleware\AuthMiddleware::class,
-        'apiAuthMiddleware' => \PTC\App\Middleware\ApiAuthMiddleware::class
+        'apiAuthMiddleware' => \PTC\App\Middleware\ApiAuthMiddleware::class,
+        'onlyVerifyPhone' => \PTC\App\Middleware\OnlyVerifyPhoneMiddleware::class,
     ]
 ];
